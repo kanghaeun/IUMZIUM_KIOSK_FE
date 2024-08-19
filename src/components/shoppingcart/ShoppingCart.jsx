198945fd4e64fea1301b06179f0fc851b2a36fb8
kanghaeun <xgkg0330@jnu.ac.kr>
2024-08-20 02:40:20 +0900

    chore: 장바구니에 들어가는 컴포넌트 import

--- src/components/shoppingcart/ShoppingCart.jsx
+++ src/components/shoppingcart/ShoppingCart.jsx
@@ -2,16 +2,36 @@ import CartClear from "./CartClear";
 import CartLits from "./CartLits";
 import CartTotal from "./CartTotal";
 import CheckoutButton from "./CheckoutButton";
+import styled from "styled-components";
 
 const ShoppingCart = () => {
   return (
     <div>
-      <CartLits />
-      <CartTotal />
-      <CartClear />
-      <CheckoutButton />
+      <ShoppingCartLayout>
+        <CartLits />
+        <CashContainer>
+          <CartTotal />
+          <CartClear />
+        </CashContainer>
+        <CheckoutButton />
+      </ShoppingCartLayout>
     </div>
   );
 };
 
 export default ShoppingCart;
+
+const ShoppingCartLayout = styled.div`
+  display: flex;
+  width: 90%;
+  height: 112px;
+  border: 1px solid;
+`;
+
+const CashContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid;
+  width: 147px;
+  height: 112px;
+`;
