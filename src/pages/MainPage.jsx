b9d93ed3f37c251c5f4dafd1ed1dc515f502e476
mlnwns <mlnwns@naver.com>
2024-08-20 00:06:31 +0900

    feat: 스타일드 컴포넌트 설치 및 적용

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+`;
+
 const MainPage = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <h1>Welcome to the Main Page</h1>
+    </Container>
+  );
 };
 
 export default MainPage;
