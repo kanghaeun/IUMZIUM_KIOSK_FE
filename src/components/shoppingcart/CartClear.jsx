d6315ee7d4182b37ba1dfc9c1d50c93cbcbdce4c
kanghaeun <xgkg0330@jnu.ac.kr>
2024-08-20 02:38:15 +0900

    style: 결제하기 컴포넌트 layout

--- src/components/shoppingcart/CartClear.jsx
+++ src/components/shoppingcart/CartClear.jsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
 const CartClear = () => {
-  return <div>전체 취소</div>;
+  return (
+    <CartClearLayout>
+      <div>전체 취소</div>
+    </CartClearLayout>
+  );
 };
 
 export default CartClear;
+const CartClearLayout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #bfbfbf;
+  height: 41px;
+  div {
+    color: #fff;
+    font-weight: 500;
+    font-size: 18px;
+  }
+`;
