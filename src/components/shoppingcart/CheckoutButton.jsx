1f2f057c2e2beb862a7dece3751e56ef80bd8068
kanghaeun <xgkg0330@jnu.ac.kr>
2024-08-20 02:40:01 +0900

    style: 결제하기 버튼 레이아웃 설정

--- src/components/shoppingcart/CheckoutButton.jsx
+++ src/components/shoppingcart/CheckoutButton.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import checkoutImage from "../../assets/checkout.png";
 const CheckoutButton = () => {
-  return <div>결제하기</div>;
+  return (
+    <ButtonContainer>
+      <CheckoutImage src={checkoutImage} alt="Checkout" />
+      <div>결제하기</div>
+    </ButtonContainer>
+  );
 };
 
 export default CheckoutButton;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 156px;
+  cursor: pointer;
+  background-color: #1b3c35;
+  div {
+    color: #fff;
+    font-size: 20px;
+  }
+`;
+
+const CheckoutImage = styled.img`
+  width: 64px; // 이미지 크기를 조절하세요
+  height: auto;
+  margin-bottom: 10px;
+`;
