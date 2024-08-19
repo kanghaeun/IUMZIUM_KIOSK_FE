324f878fdd1bc34c2091c26da837657d9e2a1bc9
mlnwns <mlnwns@naver.com>
2024-08-20 02:16:23 +0900

    feat: 음성 데이터 표시 영역

---
+++ src/components/main/Voice.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Voice = () => {
+  return <Area></Area>;
+};
+
+export default Voice;
+
+const Area = styled.div`
+  width: 820px; /* iPad Air 논리 해상도 (가로) */
+  height: 100px;
+  margin: 0 auto;
+  position: relative;
+  background-color: #1b3c35;
+  border-top: 1px solid #e0e0e0;
+`;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,17 @@
-// src/pages/App.js
 import Container from "../components/common/Container";
 import Category from "../components/main/Category";
 import ProductGrid from "../components/main/ProductGrid";
+import Voice from "../components/main/Voice";
 
 function App() {
   return (
-    <Container>
-      <Category />
-      <ProductGrid />
-    </Container>
+    <>
+      <Container>
+        <Category />
+        <ProductGrid />
+      </Container>
+      <Voice />
+    </>
   );
 }
 
