551c2587938ff9836e6c3224aa29573516ad9539
mlnwns <mlnwns@naver.com>
2024-08-19 23:24:59 +0900

    feat: react-router-dom 설치 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
 }
 
 export default App;
