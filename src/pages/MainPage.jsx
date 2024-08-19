37cc0224fbba93d25821adff672fe6b5214b8010
mlnwns <mlnwns@naver.com>
2024-08-20 02:05:40 +0900

    feat: 카테고리, 상품 목록 컴포넌트 적용

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,64 +1,15 @@
-import styled, { css } from "styled-components";
+// src/pages/App.js
 import Container from "../components/common/Container";
-import { useState } from "react";
+import Category from "../components/main/Category";
+import ProductGrid from "../components/main/ProductGrid";
 
 function App() {
-  const [activeCategory, setActiveCategory] = useState(null);
-
-  const handleCategoryClick = (category) => {
-    setActiveCategory(category);
-  };
   return (
     <Container>
-      <CategoryContainer>
-        <Category
-          isActive={activeCategory === "coffee"}
-          onClick={() => handleCategoryClick("coffee")}
-        >
-          커피
-        </Category>
-        <Category
-          isActive={activeCategory === "tea"}
-          onClick={() => handleCategoryClick("tea")}
-        >
-          차
-        </Category>
-        <Category
-          isActive={activeCategory === "ade"}
-          onClick={() => handleCategoryClick("ade")}
-        >
-          에이드
-        </Category>
-        <Category
-          isActive={activeCategory === "decaf"}
-          onClick={() => handleCategoryClick("decaf")}
-        >
-          디카페인
-        </Category>
-      </CategoryContainer>
+      <Category />
+      <ProductGrid />
     </Container>
   );
 }
 
 export default App;
-
-const CategoryContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 20px;
-`;
-
-const Category = styled.div`
-  flex: 1;
-  text-align: center;
-  padding: 10px;
-  font-size: 25px;
-  background-color: white;
-  color: black;
-  cursor: pointer;
-  ${({ isActive }) =>
-    isActive &&
-    css`
-      border-bottom: 2px solid black;
-    `}
-`;
